bd046ae2ba6c37b0b6c480b7b3181bd7a5712c5e
alfianlion <alfianlion@gmail.com>
2022-02-15 18:49:00 +0800

    movied summoner.js function into script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,17 @@
 $(document).ready(function () {
 	const reroll = config.REROLL_API;
 	const db = config.DB_API;
-	let matchList = [];
 	const user = sessionStorage.getItem("login");
-	const loginBtn = $("nav .action li a");;
+	const loginBtn = $("nav .action li a");
+	let matchList = 0;
+    let champList = new Array();
+    let content = new String();
+    let placementList = new Array();
+    let goldleftList = new Array();
+    let playerelimList = new Array();
+    let damagetoplayersList = new Array();
+    let gameinfo = JSON.parse(sessionStorage.getItem("gameInfo"));
+    let getSum = JSON.parse(sessionStorage.getItem("summonerJSON"));
 
 	hideLottie();
 	$("#region-select").html($(".region-option.active").html());
@@ -24,7 +32,10 @@ $(document).ready(function () {
 		const riot_api_url = `https://${region}.api.riotgames.com`;
 		const rank = $(".rank-option.active").attr("data-rank");
 		getLeaderboard(riot_api_url, rank);
-	}
+	} else if(location.href.includes("summoner.html")){
+        queryChampList();
+        queryChampPic();
+    }
 
 	function showLottie() {
 		$(".lottie").show();
@@ -296,6 +307,73 @@ $(document).ready(function () {
             sessionStorage.setItem('gameInfo', JSON.stringify(storedProductList));
             // Bring user to next page
         });
+    }
+
+    function queryChampList(){
+        for (let a = 0; a < gameinfo.length; a++) {
+            const game = gameinfo[a];
+            for (let i = 0; i < game.info.participants.length; i++) {
+                const element = game.info.participants[i];
+                if  (element.puuid === getSum.puuid){
+                    matchList++;
+                    placementList.push(element.placement)
+                    goldleftList.push(element.gold_left)
+                    playerelimList.push(element.players_eliminated)
+                    damagetoplayersList.push(element.total_damage_to_players)
+                    for (let a = 0; a < element.units.length; a++) {
+                        const champion = element.units[a];
+                        let champName = champion.character_id.split("_");
+                        champList.push(champName[1])
+                    }
+                    champList.push("*")
+                    break;
+                }
+            }
+        }
+    }
+    
+    function queryChampPic(){
+        let index = 0;
+        let b = 0;
+        for (let i = 0; i < matchList; i++) {
+            if (placementList.length !== index){
+                content = `${content}
+                <tr>${getSum.character_id}</tr>
+                <td>${placementList[index]}</td>
+                <td><div class="champPics"></div></td>
+                <td>${goldleftList[index]}</td>
+                <td>${playerelimList[index]}</td>
+                <td>${damagetoplayersList[index]}</td>
+                `;
+                index++;
+            } else {
+                break;
+            }          
+            $("#sumData tbody").html(content)
+            $("#summonerName").html(getSum.name)
+            const d = new Date();
+            $("#updateTime").html(`${d.getDate()}/${d.getMonth()}/${d.getFullYear()} ${d.getHours() > 12 ? d.getHours() - 12 : d.getHours()}:${d.getMinutes() < 10 ? `0${d.getMinutes()}` : d.getMinutes()} ${d.getHours() > 12 ? "pm" : "am"}`);
+    
+        }
+        
+        for (let a = 0; a < champList.length; a++) {
+            const champion = champList[a];
+            let images = $(`<img alt="${champion}" width="30px" height="30px" />`).attr("src",`assets/img/champion/${champion}.png`);
+            if(b == matchList){
+                break;
+            } else{
+                if(champList[a] !== "*"){
+                    images.appendTo($("#sumData tbody").find(".champPics")[b]);
+                } else {
+                    b++;
+                }
+            }
+        }
+    
+        const champion = champList[1];
+        let profile = $(`<img alt="${champion}" class="profile-icon"/>`).attr("src",`assets/img/champion/${champion}.png`);
+        $(".summonerInfo").prepend(profile);
+        $(".lottie").hide();
     }
 
     async function getUsers() {
